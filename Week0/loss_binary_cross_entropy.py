d2fe2df9d3e68aa2fa3400797ccbfbc061fc041d
k-rt-k <94541845+k-rt-k@users.noreply.github.com>
2022-05-04 22:56:06 +0530

    add code 1/n - loss ad ipynbs

---
+++ Week0/loss_binary_cross_entropy.py
@@ -0,0 +1,29 @@
+# Cross entropy loss
+from sklearn.datasets import make_circles
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from matplotlib import pyplot
+# generate 2d classification dataset
+X, y = make_circles(n_samples=5000, noise=0.1, random_state=1)
+# split into train and test
+train1 = 2500
+trainX, testX = X[:train1, :], X[train1:, :]
+trainy, testy = y[:train1], y[train1:]
+# define model
+model = Sequential()
+model.add(Dense(50, input_dim=2, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(1, activation='sigmoid'))
+opt = SGD(lr=0.01, momentum=0.9)
+model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+# fit model
+history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=50, verbose=0)
+# evaluate the model
+train_acc = model.evaluate(trainX, trainy, verbose=0)
+test_acc = model.evaluate(testX, testy, verbose=0)
+# plot loss during training
+pyplot.title('Binary Cross Entropy Loss')
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
---
+++ Week0/loss_categorical_cross_entropy.py
@@ -0,0 +1,33 @@
+# Multi-class Cross-entropy loss
+from sklearn.datasets import make_blobs
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.utils import to_categorical
+from matplotlib import pyplot
+# generate 2d classification dataset
+X, y = make_blobs(n_samples=5000, centers=3, n_features=2, cluster_std=2, random_state=2)
+# one hot encode output variable
+y = to_categorical(y)
+# split into train and test
+train1 = 500
+trainX, testX = X[:train1, :], X[train1:, :]
+trainy, testy = y[:train1], y[train1:]
+# define model
+model = Sequential()
+model.add(Dense(50, input_dim=2, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(3, activation='softmax'))
+# compile model
+opt = SGD(lr=0.01, momentum=0.9)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+# fit model
+history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=50, verbose=0)
+# evaluate the model
+train_acc = model.evaluate(trainX, trainy, verbose=0)
+test_acc = model.evaluate(testX, testy, verbose=0)
+# plot loss during training
+pyplot.title('Categorical Cross Entropy')
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
