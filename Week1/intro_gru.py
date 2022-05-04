2c5a6fafb703bf3471575b261d0298e5971c39e5
k-rt-k <94541845+k-rt-k@users.noreply.github.com>
2022-05-04 23:04:06 +0530

    add code 2/n rnn/lstm/gru init

---
+++ Week1/intro_gru.py
@@ -0,0 +1,63 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+#
+imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+#
+train_data, test_data = imdb['train'], imdb['test']
+#
+training_sentences = []
+training_labels = []
+testing_sentences = []
+testing_labels = []
+for s,l in train_data:
+    training_sentences.append(str(s.numpy()))
+    training_labels.append(l.numpy())
+for s,l in test_data:
+    testing_sentences.append(str(s.numpy()))
+    testing_labels.append(l.numpy())
+#
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
+#
+vocab_size = 10000
+embedding_dim=16
+max_length = 120
+trunc_type= 'post'
+oov_tok="<OOV>"
+#
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded = pad_sequences(sequences, maxlen=max_length, 
+                       truncating = trunc_type)
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length)
+#
+#
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                             input_length=max_length),
+    tf.keras.layers.Bidirectional(tf.keras.layers.GRU(32)),
+    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+#model.summary()
+model.compile(loss="binary_crossentropy",
+             optimizer='adam',
+             metrics=['accuracy'])
+history=model.fit(padded, training_labels_final, epochs=num_epochs,
+            validation_data = (testing_padded,testing_labels_final))
+import matplotlib.pyplot as plt
+def plot_graphs(history, string):
+    plt.plot(history.history[string])
+    plt.plot(history.history['val_'+string])
+    plt.xlabel("Epochs")
+    plt.ylabel(string)
+    plt.legend([string, 'val_'+string])
+    plt.show()
+plot_graphs(history, 'accuracy')
+plot_graphs(history, 'loss')
